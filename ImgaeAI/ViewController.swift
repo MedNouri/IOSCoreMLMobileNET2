4ebce9281354d891db7283dcd4fe244026ea8aaf
Mohamed Nouri <mohamedIOs@macbook-pro-de-selma.local>
2019-08-12 11:39:27 +0100

    Initial Commit

---
+++ ImgaeAI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImgaeAI
+//
+//  Created by Mohamed  Nouri on 12/08/2019.
+//  Copyright © 2019 Mohamed  Nouri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
